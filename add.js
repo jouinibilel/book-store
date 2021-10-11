dfce6e2b3a9c0f361b93ba8cb6ef1e5cc2d9c079
jouinibilel <bileljouinipro@hotmail.com>
2021-10-11 09:51:41 +0200

    book store ready

--- add.js
+++ add.js
@@ -13,6 +13,8 @@ function addbook()
     var o = Book($("#name").val(), $("#img").val(),$("#price").val())
     booklist.push(o)
     console.log(booklist)
+    console.log(o)
+
     $('.adding').append("<h1>" + o.name + "</h1>")
     $('.adding').append("<img src="+"'"+o.img+"'"+"/>")
     $('.adding').append("<h2>"+ o.price +"</h2>")
